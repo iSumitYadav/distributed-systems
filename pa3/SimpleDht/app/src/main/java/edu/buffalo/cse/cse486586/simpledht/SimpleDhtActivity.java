6f1fc97fb8b17c2c66054c69c47f300690cb4d3d
Sumit Kumar <sumityadavcools@gmail.com>
2020-03-23 18:20:34 -0400

    PA3 | Basic Content Provider Up

--- pa3/SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
+++ pa3/SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
@@ -15,8 +15,9 @@ public class SimpleDhtActivity extends Activity {
         
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        findViewById(R.id.button3).setOnClickListener(
-                new OnTestClickListener(tv, getContentResolver()));
+        findViewById(R.id.button3).setOnClickListener(new OnTestClickListener(tv, getContentResolver()));
+
+
     }
 
     @Override
--- pa3/SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ pa3/SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -1,15 +1,29 @@
 package edu.buffalo.cse.cse486586.simpledht;
 
+import java.util.Formatter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.database.Cursor;
+
 import android.net.Uri;
+import android.util.Log;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.content.ContentProvider;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 
 public class SimpleDhtProvider extends ContentProvider {
+    static final Uri CONTENT_URI = Uri.parse("content://edu.buffalo.cse.cse486586.simpledht.provider");
+
+    private SQLiteDatabase db;
+    public static final String TABLE_NAME = "messages";
+    public static final String COLUMN_NAME_KEY = "key";
+    public static final String COLUMN_NAME_VALUE = "value";
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -26,20 +40,48 @@ public class SimpleDhtProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO Auto-generated method stub
-        return null;
+        db.insert(TABLE_NAME, null, values);
+
+        return uri;
     }
 
     @Override
     public boolean onCreate() {
         // TODO Auto-generated method stub
+        SimpleDHTDBHelper DBHelper = new SimpleDHTDBHelper(getContext());
+
+        db = DBHelper.getWritableDatabase();
+        if(db != null){
+            return true;
+        }
+
         return false;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-            String sortOrder) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         // TODO Auto-generated method stub
-        return null;
+
+        String [] selectionArgss = new String[]{selection};
+
+        selection = COLUMN_NAME_KEY + "=?";
+
+        Cursor cursor = db.query(
+                TABLE_NAME,
+                projection,
+                selection,
+                selectionArgss,
+                null,
+                null,
+                sortOrder,
+                String.valueOf(1)
+        );
+
+        Log.d("qKEY", selectionArgss[0].toString());
+        cursor.moveToFirst();
+        Log.d("qVALUE", cursor.getString(1));
+
+        return cursor;
     }
 
     @Override
@@ -56,5 +98,27 @@ public class SimpleDhtProvider extends ContentProvider {
             formatter.format("%02x", b);
         }
         return formatter.toString();
+    }
+
+    public class SimpleDHTDBHelper extends SQLiteOpenHelper{
+        public static final int DATABASE_VERSION = 1;
+        private final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_NAME_KEY + " TEXT," + COLUMN_NAME_VALUE + " TEXT)";
+
+        public SimpleDHTDBHelper(Context context) {
+            // Made in-memory database by not specifying 2nd arg name with
+            // DBName.
+            super(context, null, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_TABLE);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
     }
 }
